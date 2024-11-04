b9ab826729ebb79a677fb11f7e81c2037fdef67a
LakshmiSoundarya <Lakshmisoundarya.maganti@nationwidechildrens.org>
2024-11-04 14:33:25 -0500

    adding wcmstests

---
+++ src/test/java/stepDefinitions/UtilClass.java
@@ -0,0 +1,35 @@
+package stepDefinitions;
+
+
+
+public class UtilClass {
+
+//	public WebDriver driver;
+//	
+//	public Properties p;
+	
+//	@Before
+//	public void setup() throws IOException
+//	{
+//		driver = new ChromeDriver();
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//		driver.manage().window().maximize();
+//		FileReader file = new FileReader("./src/test/resources/application.properties");
+//		p = new Properties();
+//		p.load(file);
+//	}
+//	
+//	public RandomStringUtils randomeString() {
+//		RandomStringUtils generatestring = RandomStringUtils.secure();
+//		return generatestring;
+//	}
+//	
+//	@After
+//	public void teardown() {
+//		driver.quit();
+//	}
+//	
+	
+	
+	
+}
---
+++ src/test/java/testRunner/TestRunner.java
@@ -0,0 +1,16 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features=".//src/test/resources/Features/Form.feature",
+	glue="stepDefinitions",
+	dryRun=true,
+	monochrome=true,
+	plugin={"pretty","html:.//target/test-output"})
+public class TestRunner {
+
+}
